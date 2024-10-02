01564b0ca45c1eeef6e32626a4fa2796b1a06bdc
v8040 <59680407+v8040@users.noreply.github.com>
2024-10-03 01:14:46 +0800

    Preset dnsproxy

--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 mkdir -p files/root
-pushd files/root
 
 # Clone oh-my-zsh repository
-git clone -q --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ./.oh-my-zsh
+git clone -q --depth=1 https://github.com/ohmyzsh/ohmyzsh.git files/root/.oh-my-zsh
 
 # Install extra plugins
-git clone -q --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone -q --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone -q --depth=1 https://github.com/zsh-users/zsh-completions.git ./.oh-my-zsh/custom/plugins/zsh-completions
+git clone -q --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git files/root/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone -q --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git files/root/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # Get .zshrc dotfile
-cp $GITHUB_WORKSPACE/scripts/.zshrc .
-
-popd
+cp -f $GITHUB_WORKSPACE/scripts/.zshrc files/root/.zshrc
 
 echo -e "\e[32m$0 [DONE]\e[0m"
